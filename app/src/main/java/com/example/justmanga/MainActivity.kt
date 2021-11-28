7ac9df9b8b42607f317b6a2604045f80892e8ffc
Dimitri Tskitishvili <tskitishvili01@gmail.com>
2021-11-28 22:54:55 +0400

    Initial commit

---
+++ app/src/main/java/com/example/justmanga/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.justmanga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar()?.hide(); //hide the title bar
+        setContentView(R.layout.activity_main)
+
+    }
+}
